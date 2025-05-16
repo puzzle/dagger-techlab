ae02f6a14b1ddc72fe0be7bee2910278636ab559
Christoph Raaflaub <raaflaub@puzzle.ch>
2025-05-16 08:37:12 +0200

    add kubeconfig vars

--- scripts/create_kubeconfig.sh
+++ scripts/create_kubeconfig.sh
@@ -3,8 +3,10 @@
 set -euo pipefail
 
 : "${KUBE_CONFIG:?KUBE_CONFIG environment variable is required}"
+: "${KUBE_CONFIG_PATH:?KUBE_CONFIG_PATH environment variable is required}"
+: "${KUBE_CONFIG_FILENAME:?KUBE_CONFIG_FILENAME environment variable is required}"
 
-echo "📄 Writing kubeconfig to \$HOME/.kube/config..."
-mkdir -p "$HOME/.kube"
-echo "$KUBE_CONFIG" > "$HOME/.kube/config"
+echo "📄 Writing kubeconfig to \$KUBE_CONFIG_PATH/\$KUBE_CONFIG_FILENAME ($KUBE_CONFIG_PATH/$KUBE_CONFIG_FILENAME) ..."
+mkdir -p $KUBE_CONFIG_PATH
+echo "$KUBE_CONFIG" > "$KUBE_CONFIG_PATH/$KUBE_CONFIG_FILENAME"
 echo "✅ Kubeconfig written successfully."
--- scripts/deploy_helm_release.sh
+++ scripts/deploy_helm_release.sh
@@ -6,13 +6,15 @@ set -euo pipefail
 : "${HELM_RELEASE:?HELM_RELEASE is required}"
 : "${NAMESPACE:?NAMESPACE is required}"
 : "${TRAINING_VERSION:?TRAINING_VERSION is required}"
+: "${KUBE_CONFIG_PATH:?KUBE_CONFIG_PATH environment variable is required}"
+: "${KUBE_CONFIG_FILENAME:?KUBE_CONFIG_FILENAME environment variable is required}"
 
 echo "🚀 Deploying Helm release '$HELM_RELEASE' into namespace '$NAMESPACE'..."
 
 helm upgrade "$HELM_RELEASE" acend-training-chart \
   --install \
   --wait \
-  --kubeconfig "$HOME/.kube/config" \
+  --kubeconfig "$KUBE_CONFIG_PATH/$KUBE_CONFIG_FILENAME" \
   --namespace "$NAMESPACE" \
   --set=app.name="$HELM_RELEASE" \
   --set=app.version="$TRAINING_VERSION" \
